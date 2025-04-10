04d5d03a63e9a7b4ed3146e2aae3589474d5abf9
trantin12345 <trantin505ad@gmail.com>
2025-04-10 10:25:25 +0700

    add

---
+++ Tinh/src/Tinhtoan.java
@@ -0,0 +1,10 @@
+
+public class Tinhtoan {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10; 
+        int c = a + b; 
+
+        System.out.println("Tổng của a và b là: " + c);
+    }
+}
